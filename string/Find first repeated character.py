b4d7ec19e6f9ec4ff39018863f99d80c10586f47
Priyesh Singh <mrpriyesh037@gmail.com>
2024-09-25 19:53:01 +0530

    Update and rename Find first repeated character. to Find first repeated character.py

--- string/Find first repeated character.py
+++ string/Find first repeated character.py
@@ -1,14 +1,10 @@
 Find first repeated character
 Difficulty: EasyAccuracy: 41.97%Submissions: 83K+Points: 2
 Given a string S. The task is to find the first repeated character in it. We need to find the character that occurs more than once and whose index of second occurrence is smallest. S contains only lowercase letters.
-
- 
-
 Example 1:
 Input: S="geeksforgeeks"
 Output: e
 Explanation: 'e' repeats at third position.
- 
 
 Example 2:
 Input: S="hellogeeks"
@@ -16,7 +12,6 @@ Output: l
 Explanation: 'l' repeats at fourth position.
 
 code=
-
 class Solution:
     def firstRepChar(self, s):
         seen=set()
