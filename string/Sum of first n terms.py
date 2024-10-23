c1cf473705ce91bd3e045465a664f8571635ec6b
Priyesh Singh <mrpriyesh037@gmail.com>
2024-10-23 19:01:21 +0530

    Sum of first n terms.py

---
+++ string/Sum of first n terms.py
@@ -0,0 +1,17 @@
+Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+
+Examples:
+
+Input: n = 5
+Output: 225
+Explanation: 13 + 23 + 33 + 43 + 53 = 225
+Input: n = 7
+Output: 784
+Explanation: 13 + 23 + 33 + 43 + 53 + 63 + 73 = 784
+
+
+code=
+class Solution:
+    def sumOfSeries(self, n):
+         return (n * (n + 1) // 2) ** 2
+        
