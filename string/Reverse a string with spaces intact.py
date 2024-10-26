09bde5b01832d7f20c5809056cece43d04a9e812
Priyesh Singh <mrpriyesh037@gmail.com>
2024-10-26 22:11:14 +0530

    Reverse a string with spaces intact.py

---
+++ string/Reverse a string with spaces intact.py
@@ -0,0 +1,16 @@
+Given a string, your task is to reverse the string keeping the spaces intact to their positions.
+
+Example 1:
+
+Input:
+S = "Help others"
+Output: sreh topleH
+Explanation: The space is intact at index 4
+while all other characters are reversed.
+Example 2:
+
+Input: 
+S = "geeksforgeeks"
+Output: skeegrofskeeg
+Explanation: No space present, hence the
+entire string is reversed.
